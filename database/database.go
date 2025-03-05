ea6d6302e0c8c6a392775383bfce6acd567f75dd
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-03-05 21:26:20 +0000

    add session table

--- database/database.go
+++ database/database.go
@@ -90,3 +90,20 @@ func CategoriesTable(db *sql.DB) {
 		log.Fatal("Error creating categories table:", err)
 	}
 }
+
+func Session(db *sql.DB) {
+	query := `
+	CREATE TABLE sessions (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		session_token TEXT UNIQUE NOT NULL,
+		expires_at DATETIME NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	);
+`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating categories table:", err)
+	}
+}
